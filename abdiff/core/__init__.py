6827ee60a942456d4a14b4bd85469b305807cfdd
Graham Hukill <ghukill@gmail.com>
2024-10-15 15:30:54 -0400

    Alignment from branch rebasing

--- abdiff/core/__init__.py
+++ abdiff/core/__init__.py
@@ -4,9 +4,9 @@ All primary functions used by CLI are importable from here.
 """
 
 from abdiff.core.build_ab_images import build_ab_images
-from abdiff.core.collate_ab_transforms import collate_ab_transforms
 from abdiff.core.calc_ab_diffs import calc_ab_diffs
 from abdiff.core.calc_ab_metrics import calc_ab_metrics
+from abdiff.core.collate_ab_transforms import collate_ab_transforms
 from abdiff.core.init_job import init_job
 from abdiff.core.init_run import init_run
 from abdiff.core.run_ab_transforms import run_ab_transforms
