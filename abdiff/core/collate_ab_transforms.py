a329a9942512c71b96c22607f1c815b698c08dad
Graham Hukill <ghukill@gmail.com>
2024-10-29 13:13:57 -0400

    Dedupe records as part of collate_ab_transforms
    Why these changes are being introduced:
    
    Ideally, we could provide files to ABDiff in the way they
    accumulate in S3 where full runs are followed by dailes,
    sometimes there are deletes, etc.  But in doing so, we would
    only end up with the most recent, non-deleted version of
    the record in the final dataset to analyze.
    
    How this addresses that need:
    * Adds step in collate_ab_transforms to dedupe records
    based on timdex_record_id, run_date, run_type, and action
    
    Side effects of this change:
    * Analysis will only contain the most recent version of a record
    even if the record is duplicated amongst input files.
    
    Relevant ticket(s):
    * https://mitlibraries.atlassian.net/browse/TIMX-371

--- abdiff/core/collate_ab_transforms.py
+++ abdiff/core/collate_ab_transforms.py
@@ -1,3 +1,5 @@
+# ruff: noqa: TRY003
+
 import itertools
 import json
 import logging
@@ -20,15 +22,21 @@ TRANSFORMED_DATASET_SCHEMA = pa.schema(
     (
         pa.field("timdex_record_id", pa.string()),
         pa.field("source", pa.string()),
+        pa.field("run_date", pa.date32()),
+        pa.field("run_type", pa.string()),
+        pa.field("action", pa.string()),
         pa.field("record", pa.binary()),
         pa.field("version", pa.string()),
         pa.field("transformed_file_name", pa.string()),
     )
 )
-JOINED_DATASET_SCHEMA = pa.schema(
+COLLATED_DATASET_SCHEMA = pa.schema(
     (
         pa.field("timdex_record_id", pa.string()),
         pa.field("source", pa.string()),
+        pa.field("run_date", pa.date32()),
+        pa.field("run_type", pa.string()),
+        pa.field("action", pa.string()),
         pa.field("record_a", pa.binary()),
         pa.field("record_b", pa.binary()),
     )
@@ -40,18 +48,19 @@ def collate_ab_transforms(
 ) -> str:
     """Collates A/B transformed files into a Parquet dataset.
 
-    This process can be summarized into two (2) important steps:
+    This process can be summarized into two (3) important steps:
         1. Write all transformed JSON records into a temporary Parquet dataset
            partitioned by the transformed file name.
         2. For every transformed file, use DuckDB to join A/B Parquet tables
            using the TIMDEX record ID and write joined records to a Parquet dataset.
-
-    This function (and its subfunctions) uses DuckDB, generators, and batching to
-    write records to Parquet datasets in a memory-efficient manner.
+        3. Dedupe joined records to ensure that only the most recent, not "deleted"
+           timdex_record_id is present in final output.
     """
     transformed_dataset_path = tempfile.TemporaryDirectory()
+    joined_dataset_path = tempfile.TemporaryDirectory()
     collated_dataset_path = str(Path(run_directory) / "collated")
 
+    # build temporary transformed dataset
     transformed_written_files = write_to_dataset(
         get_transformed_batches_iter(run_directory, ab_transformed_file_lists),
         schema=TRANSFORMED_DATASET_SCHEMA,
@@ -62,14 +71,29 @@ def collate_ab_transforms(
         f"Wrote {len(transformed_written_files)} parquet file(s) to transformed dataset"
     )
 
+    # build temporary collated dataset
     joined_written_files = write_to_dataset(
         get_joined_batches_iter(transformed_dataset_path.name),
-        base_dir=collated_dataset_path,
-        schema=JOINED_DATASET_SCHEMA,
+        base_dir=joined_dataset_path.name,
+        schema=COLLATED_DATASET_SCHEMA,
     )
     logger.info(f"Wrote {len(joined_written_files)} parquet file(s) to collated dataset")
 
+    # build final deduped and collated dataset
+    deduped_written_files = write_to_dataset(
+        get_deduped_batches_iter(joined_dataset_path.name),
+        base_dir=collated_dataset_path,
+        schema=COLLATED_DATASET_SCHEMA,
+    )
+    logger.info(
+        f"Wrote {len(deduped_written_files)} parquet file(s) to deduped collated dataset"
+    )
+
     validate_output(collated_dataset_path)
+
+    # ensure temporary artifacts removed
+    transformed_dataset_path.cleanup()
+    joined_dataset_path.cleanup()
 
     return collated_dataset_path
 
@@ -85,7 +109,9 @@ def get_transformed_records_iter(
 
     * timdex_record_id: The TIMDEX record ID.
     * source: The shorthand name of the source as denoted in by Transmogrifier
-      (see https://github.com/MITLibraries/transmogrifier/blob/main/transmogrifier/config.py).
+    * run_date: Run date from TIMDEX ETL
+    * run_type: "full" or "daily"
+    * action: "index" or "delete"
     * record: The TIMDEX record serialized to a JSON string then encoded to bytes.
     * version: The version of the transform, parsed from the absolute filepath to a
       transformed file.
@@ -98,6 +124,9 @@ def get_transformed_records_iter(
             yield {
                 "timdex_record_id": record["timdex_record_id"],
                 "source": filename_details["source"],
+                "run_date": filename_details["run-date"],  # use underscore for DuckDB
+                "run_type": filename_details["run-type"],  # use underscore for DuckDB
+                "action": filename_details["action"],
                 "record": json.dumps(record).encode(),
                 "version": version,
                 "transformed_file_name": transformed_file.split("/")[-1],
@@ -192,6 +221,9 @@ def get_joined_batches_iter(dataset_directory: str) -> Generator[pa.RecordBatch]
                 SELECT
                     COALESCE(a.timdex_record_id, b.timdex_record_id) timdex_record_id,
                     COALESCE(a.source, b.source) source,
+                    COALESCE(a.run_date, b.run_date) run_date,
+                    COALESCE(a.run_type, b.run_type) run_type,
+                    COALESCE(a.action, b.action) "action",
                     a.record as record_a,
                     b.record as record_b
                 FROM a
@@ -210,6 +242,64 @@ def get_joined_batches_iter(dataset_directory: str) -> Generator[pa.RecordBatch]
                     break
 
 
+def get_deduped_batches_iter(dataset_directory: str) -> Generator[pa.RecordBatch]:
+    """Yield pyarrow.RecordBatch objects of deduped rows from the joined dataset.
+
+    ABDiff should be able to handle many input files, where a single timdex_record_id may
+    be duplicated across multiple files ("full" vs "daily" runs, incrementing date runs,
+    etc.)
+
+    This function writes the final dataset by deduping records from the temporary collated
+    dataset, given the following logic:
+        - use the MOST RECENT record based on 'run_date'
+        - if the MOST RECENT record is action='delete', then omit record entirely
+
+    The same mechanism is used by get_joined_batches_iter() to perform a DuckDB query then
+    stream write batches to a parquet dataset.
+    """
+    with duckdb.connect(":memory:") as con:
+
+        results = con.execute(
+            """
+            WITH collated as (
+              select * from read_parquet($collated_parquet_glob, hive_partitioning=true)
+            ),
+            latest_records AS (
+                SELECT
+                    *,
+                    ROW_NUMBER() OVER (
+                        PARTITION BY timdex_record_id
+                        ORDER BY run_date DESC
+                    ) AS rn
+                FROM collated
+            ),
+            deduped_records AS (
+                SELECT *
+                FROM latest_records
+                WHERE rn = 1 AND action != 'delete'
+            )
+            SELECT
+                timdex_record_id,
+                source,
+                run_date,
+                run_type,
+                action,
+                record_a,
+                record_b
+            FROM deduped_records;
+            """,
+            {
+                "collated_parquet_glob": f"{dataset_directory}/**/*.parquet",
+            },
+        ).fetch_record_batch(READ_BATCH_SIZE)
+
+        while True:
+            try:
+                yield results.read_next_batch()
+            except StopIteration:
+                break  # pragma: nocover
+
+
 def validate_output(dataset_path: str) -> None:
     """Validate the output of collate_ab_transforms.
 
@@ -217,8 +307,14 @@ def validate_output(dataset_path: str) -> None:
     and whether any or both 'record_a' or 'record_b' columns are
     totally empty.
     """
+
+    def fetch_single_value(query: str) -> int:
+        result = con.execute(query).fetchone()
+        if result is None:
+            raise RuntimeError(f"Query returned no results: {query}")  # pragma: nocover
+        return int(result[0])
+
     with duckdb.connect(":memory:") as con:
-        # create view of collated table
         con.execute(
             f"""
             CREATE VIEW collated AS (
@@ -228,29 +324,41 @@ def validate_output(dataset_path: str) -> None:
         )
 
         # check if the table is empty
-        record_count = con.execute("SELECT COUNT(*) FROM collated").fetchone()[0]  # type: ignore[index]
+        record_count = fetch_single_value("SELECT COUNT(*) FROM collated")
         if record_count == 0:
-            raise OutputValidationError(  # noqa: TRY003
+            raise OutputValidationError(
                 "The collated dataset does not contain any records."
             )
 
         # check if any of the 'record_*' columns are empty
-        record_a_null_count = con.execute(
+        record_a_null_count = fetch_single_value(
             "SELECT COUNT(*) FROM collated WHERE record_a ISNULL"
-        ).fetchone()[
-            0
-        ]  # type: ignore[index]
-
-        record_b_null_count = con.execute(
+        )
+        record_b_null_count = fetch_single_value(
             "SELECT COUNT(*) FROM collated WHERE record_b ISNULL"
-        ).fetchone()[
-            0
-        ]  # type: ignore[index]
+        )
 
         if record_count in {record_a_null_count, record_b_null_count}:
-            raise OutputValidationError(  # noqa: TRY003
+            raise OutputValidationError(
                 "At least one or both record column(s) ['record_a', 'record_b'] "
                 "in the collated dataset are empty."
+            )
+
+        # check that timdex_record_id column is unique
+        non_unique_count = fetch_single_value(
+            """
+            SELECT COUNT(*)
+            FROM (
+                SELECT timdex_record_id
+                FROM collated
+                GROUP BY timdex_record_id
+                HAVING COUNT(*) > 1
+            ) as duplicates;
+        """
+        )
+        if non_unique_count > 0:
+            raise OutputValidationError(
+                "The collated dataset contains duplicate 'timdex_record_id' records."
             )
 
 
@@ -261,8 +369,6 @@ def get_transform_version(transformed_filepath: str) -> str:
         transformed_filepath,
     )
     if not match_result:
-        raise ValueError(  # noqa: TRY003
-            f"Transformed filepath is invalid: {transformed_filepath}."
-        )
+        raise ValueError(f"Transformed filepath is invalid: {transformed_filepath}.")
 
     return match_result.groups()[0]
